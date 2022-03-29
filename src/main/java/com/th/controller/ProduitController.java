5ce109ce38f86969401e4c453c3b9da6749a3699
khalil <khalillakhdharatc@gmail.com>
2022-03-29 12:34:21 +0100

    Update ProduitController.java

--- src/main/java/com/th/controller/ProduitController.java
+++ src/main/java/com/th/controller/ProduitController.java
@@ -1,10 +1,28 @@
 package com.th.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.th.entities.Produit;
+import com.th.repositories.ProduitRepository;
 
 @RestController
 @RequestMapping("produit")
 public class ProduitController {
+	@Autowired
+	ProduitRepository produitRepository;
+	
+	public List<Produit> getAll()
+	{
+		return produitRepository.findAll();
+		
+	}
+	
 
+	
+	
+	
 }
